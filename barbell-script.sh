ea64c882471409d05acd4b8f26483f1995c8825f
Juuso Haavisto <juuso@ponkila.com>
2023-09-07 19:55:50 +0100

    flake: use packages for init neovim, barbell

--- barbell-script.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-cbqn barbell.bqn $1
--- flake.nix
+++ flake.nix
@@ -1,24 +1,20 @@
 {
 
   inputs = {
-    bqnlsp.inputs.nixpkgs.follows = "nixpkgs";
-    bqnlsp.url = "sourcehut:~detegr/bqnlsp";
+    barbell.inputs.nixpkgs.follows = "nixpkgs";
+    barbell.url = "github:jhvst/barbell?dir=packages/barbell";
     devenv.url = "github:cachix/devenv";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    juuso.inputs.nixpkgs.follows = "nixpkgs";
-    juuso.url = "github:jhvst/nix-config";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixvim.url = "github:nix-community/nixvim";
+    neovim.inputs.nixpkgs.follows = "nixpkgs";
+    neovim.url = "github:jhvst/barbell?dir=packages/neovim";
   };
 
   outputs =
     inputs@{ self
-    , bqnlsp
     , devenv
     , flake-parts
-    , juuso
     , nixpkgs
-    , nixvim
     , ...
     }:
     flake-parts.lib.mkFlake { inherit inputs; } {
@@ -28,106 +24,20 @@
         inputs.devenv.flakeModule
       ];
 
-      perSystem = { pkgs, lib, config, system, ... }:
-        let
-          name = "barbell";
-          barbell-script = (pkgs.writeScriptBin name (builtins.readFile ./barbell-script.sh)).overrideAttrs (old: {
-            src = ./.;
-            buildCommand = ''
-              ${old.buildCommand}
-              patchShebangs $out
-              cp $src/barbell.bqn $out/bin
-              substituteInPlace $out/bin/barbell \
-                --replace barbell.bqn $out/bin/barbell.bqn
-            '';
-          });
-        in
-        {
+      perSystem = { pkgs, lib, config, system, inputs', ... }: {
 
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            overlays = [
-              inputs.juuso.overlays.default
-            ];
-            config = { };
-          };
+        packages.barbell = inputs'.barbell.packages.default;
+        packages.neovim = inputs'.neovim.packages.default;
 
-          packages.barbell = pkgs.symlinkJoin {
-            name = name;
-            paths = [ barbell-script ] ++ [ pkgs.cbqn ];
-            buildInputs = with pkgs; [ makeWrapper ];
-            postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
-          };
-
-          packages.neovim = nixvim.legacyPackages.${system}.makeNixvimWithModule {
-            inherit pkgs;
-            module = {
-              imports = [
-                inputs.juuso.outputs.nixosModules.neovim
-              ];
-              extraPackages = with pkgs; [
-                cbqn # bqnlsp assumes cbqn in path
-              ];
-              extraConfigVim = ''
-                au BufRead,BufNewFile *.bqn setf bqn
-                au BufRead,BufNewFile * if getline(1) =~ '^#!.*bqn$' | setf bqn | endif
-              '';
-              plugins.lsp = {
-                enable = true;
-                preConfig = ''
-                  local configs = require('lspconfig.configs')
-                  local util = require('lspconfig.util')
-
-                  if not configs.bqnlsp then
-                    configs.bqnlsp = {
-                      default_config = {
-                        cmd = { 'bqnlsp' },
-                        cmd_env = {},
-                        filetypes = { 'bqn' },
-                        root_dir = util.find_git_ancestor,
-                        single_file_support = false,
-                      },
-                      docs = {
-                        description = [[ BQN Language Server ]],
-                        default_config = {
-                          root_dir = [[util.find_git_ancestor]],
-                        },
-                      },
-                    }
-                  end
-                '';
-              };
-              extraPlugins = [
-                inputs.bqnlsp.packages.${system}.lsp
-                (pkgs.vimUtils.buildVimPluginFrom2Nix {
-                  pname = "bqn-vim";
-                  version = pkgs.mbqn.version;
-                  src = pkgs.mbqn.src;
-                  sourceRoot = "source/editors/vim";
-                  meta.homepage = "https://github.com/mlochbaum/BQN/editors/vim";
-                })
-                (pkgs.vimUtils.buildVimPluginFrom2Nix {
-                  pname = "nvim-bqn";
-                  version = "unstable";
-                  src = builtins.fetchGit {
-                    url = "https://git.sr.ht/~detegr/nvim-bqn";
-                    rev = "bbe1a8d93f490d79e55dd0ddf22dc1c43e710eb3";
-                  };
-                  meta.homepage = "https://git.sr.ht/~detegr/nvim-bqn/";
-                })
-              ];
-            };
-          };
-
-          devenv.shells.default = {
-            packages = [
-              config.packages.barbell
-              config.packages.neovim
-            ];
-          };
-
-          packages.default = config.packages.barbell;
-
+        devenv.shells.default = {
+          packages = [
+            config.packages.barbell
+            config.packages.neovim
+          ];
         };
+
+        packages.default = config.packages.barbell;
+
+      };
     };
 }
